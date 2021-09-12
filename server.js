d1bea39b46969f74b567bf8045fbe6b7165bb2df
McKenzie Postell <mckenziepostell99@gmail.com>
2021-09-12 15:21:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
  const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-animated-01000',
-      user : 'mckenziethompson',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
 });
 
